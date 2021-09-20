a9a7fbee3da4f3b00328a1921e426a4a2994895a
Noriyuki Abe <noriyukipy@gmail.com>
2021-09-20 11:50:25 +0900

    Update script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,10 @@
-git clone https://github.com/colorfulscoop/convmodel
+if [ -e convmodel ]; then
+    cd convmodel
+    git fetch origin master
+    git merge origin/master
+    cd ..
+else
+    git clone https://github.com/colorfulscoop/convmodel
+fi
+
 mkdocs build --clean --config-file convmodel/docs/en/mkdocs.yml --site-dir ../../../docs
